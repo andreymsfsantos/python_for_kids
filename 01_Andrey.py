285c49adfb4be9ae5e4af89e16200ac7a821f1a4
andreymsfsantos <62684260+andreymsfsantos@users.noreply.github.com>
2020-03-26 01:05:47 -0300

    Add files via upload

---
+++ 01_Andrey.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Meu primeiro script
+
+# In[8]:
+
+
+4*3  + 1 # vezes
+
+
+# In[21]:
+
+
+5%3
+
+
+# In[22]:
+
+
+7//2
+
+
+# 0-0
+
+# In[23]:
+
+
+0-0
+
+
+# In[24]:
+
+
+2+5 # adição
+
+
+# In[25]:
+
+
+7/3 # divisão
+
+
+# In[26]:
+
+
+5-2 # subtração
+
+
+# In[27]:
+
+
+2*3 # mutiplicação
+
+
+# In[29]:
+
+
+5//3 * divisão que não considera os numeros decimais
+
+
+# In[ ]:
+
+
+
+
